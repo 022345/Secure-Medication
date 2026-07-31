60b53a43d5a93506f113076b532a0a63bc48ee10
Carlos <cjjosue03@gmail.com>
2026-07-31 17:39:30 -0400

    added login page and updated intern files

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import Login from './Login';
 import '../css/App.css';
 
-const API_KEY = '0c08bc18-eabe-4de3-9a11-45f7aa5b4922';
 const API_URL = 'https://secure-medication.onrender.com/medicines';
 
 const diccionarioSintomas = {
@@ -14,21 +14,24 @@ const diccionarioSintomas = {
 };
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [medicamentos, setMedicamentos] = useState([]);
   const [terminoBusqueda, setTerminoBusqueda] = useState('');
 
   useEffect(() => {
-    fetch(API_URL, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': API_KEY
-      }
-    })
-      .then((response) => response.json())
-      .then((data) => setMedicamentos(data))
-      .catch((error) => console.error('Error al conectar con la API:', error));
-  }, []);
+    if (isLoggedIn) {
+      fetch(API_URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Authorization': `Bearer ${tu_token_jwt}` 
+        }
+      })
+        .then((response) => response.json())
+        .then((data) => setMedicamentos(data))
+        .catch((error) => console.error('Error al conectar con la API:', error));
+    }
+  }, [isLoggedIn]);
 
   const medicamentosFiltrados = medicamentos.filter((med) => {
     if (!terminoBusqueda) return true;
@@ -54,11 +57,16 @@ function App() {
     });
   });
 
+  if (!isLoggedIn) {
+    return <Login onLoginSuccess={() => setIsLoggedIn(true)} />;
+  }
+
   return (
     <div className="app-container">
       <header className="header-section">
         <h1>Secure Medication</h1>
         <p>Need anything? Then please enter the medicines you need in the search bar</p>
+        <button onClick={() => setIsLoggedIn(false)} style={{ position: 'absolute', top: '20px', right: '20px' }}>Cerrar Sesión</button>
         <div className="search-box">
           <input
             type="text"
