3a615dd029a826185e5a021ac507276b8725c821
Carlos <cjjosue03@gmail.com>
2026-08-12 13:12:54 -0400

    developing commit 3

--- SecureMedication.Medicines.Api/Controllers/MedicineController.cs
+++ SecureMedication.Medicines.Api/Controllers/MedicineController.cs
@@ -1,15 +1,33 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO.Pipelines;
+using System.Net;
+
+//Need to use the namespace for the Models package in order to access the classes models 
+using SecureMedication.Medicines.Api.Models; 
+
 
 namespace SecureMedication.Medicines.Api.Controllers
 {
+    //This is the main endpoint which leads to all of the other endpoints.
+    //To access all the other endpoints need to first add this endpoint
     [Route("medicine")]
     public class MedicineController : Controller
     {
+        //To access this endpoint need to first add the prime endpoint: /medicine/home
         [Route("home")]
         public string home()
         {
             return "Api functional";
+        }
+
+        [Route("saveMedicine")]
+        //Need to be for the actual endpoint: public List<Medicine> SaveMedicines(Medicine medicine)
+        public string saveMedicines(Medicine medicine)
+        {
+            var medicineList = new List<Medicine>();
+            return "Endpoint to save medicines, WORK ON PROGRESS";
+
         }
     }
 }
--- SecureMedication.Medicines.Api/Program.cs
+++ SecureMedication.Medicines.Api/Program.cs
@@ -8,7 +8,6 @@ namespace SecureMedication.Medicines.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
 
             builder.Services.AddOpenApi();
