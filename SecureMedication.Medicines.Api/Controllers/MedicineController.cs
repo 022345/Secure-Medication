28cf9377979ebbffab69f01575f30123d173e5bb
Carlos <cjjosue03@gmail.com>
2026-08-12 12:33:48 -0400

    added description to the README

--- SecureMedication.Medicines.Api/Controllers/MedicineController.cs
+++ SecureMedication.Medicines.Api/Controllers/MedicineController.cs
@@ -3,81 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SecureMedication.Medicines.Api.Controllers
 {
+    [Route("medicine")]
     public class MedicineController : Controller
     {
-        // GET: MedicineController
-        public ActionResult Index()
+        [Route("home")]
+        public string home()
         {
-            return View();
-        }
-
-        // GET: MedicineController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: MedicineController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: MedicineController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: MedicineController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: MedicineController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: MedicineController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: MedicineController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return "Api functional";
         }
     }
 }
--- SecureMedication.Medicines.Api/Program.cs
+++ SecureMedication.Medicines.Api/Program.cs
@@ -10,7 +10,7 @@ namespace SecureMedication.Medicines.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
