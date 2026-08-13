a7c79e5479ea2ddc7eecadcd5c8a3d511348e0ae
Carlos <cjjosue03@gmail.com>
2026-08-13 17:08:01 -0400

    added the render.yaml, configured the connections and fixed reserved words issues

---
+++ SecureMedication.Users.Api/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecureMedication.Users.Api.Controllers
+{
+    [Route("gateway")]
+    public class GatewayController : Controller
+    {
+        [Route("home")]
+        public string home()
+        {
+            return "You're in the Gateway home point! Congrats it worked!";
+        }
+    }
+}
