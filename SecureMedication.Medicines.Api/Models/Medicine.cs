34dd4f0ae797745c999f387ed733ba37120c7578
Carlos <cjjosue03@gmail.com>
2026-08-14 21:46:40 -0400

    Fixed issue 2

--- SecureMedication.Medicines.Api/Models/Medicine.cs
+++ SecureMedication.Medicines.Api/Models/Medicine.cs
@@ -7,44 +7,58 @@ namespace SecureMedication.Medicines.Api.Models
     public class Medicine
     {
         // All the fields MUST be public with their { get; set; } so EF Core and JSON serializers can read/write them
-        
+
         [Key] // Primary Key
+        [Column("id")]
         public string Id { get; set; }
 
         [Required] // Sets column as NOT NULL
         [MaxLength(50)] // Defines maximum string length (VARCHAR(25))
+        [Column("name")]
         public string Name { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(225)]
+        [Column("description")]
         public string Description { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(50)]
+        [Column("brand")]
         public string Brand { get; set; } = string.Empty;
 
+        [Column("dailydosage")]
         public int DailyDosage { get; set; }
+
+        [Column("milligrams")]
         public int Milligrams { get; set; }
+
+        [Column("quantity")]
         public int Quantity { get; set; }
 
         [Required]
         [MaxLength(150)]
+        [Column("presentation")]
         public string Presentation { get; set; } = string.Empty;
 
+        [Column("servingspercontainer")]
         public long ServingsPerContainer { get; set; }
 
         // The '?' operator allows the property to be NULLABLE in SQL without throwing C# compiler warnings
+        [Column("imageurl")]
         public string? ImageUrl { get; set; }
+
+        [Column("buyinglink")]
         public string? BuyingLink { get; set; }
 
         // EF Core requires a parameterless constructor to instantiate the model when querying the DB
-        public Medicine() 
+        public Medicine()
         {
-        
+
         }
 
-        public Medicine(string id, string name, string description, string brand, 
-            int dailyDosage, int milligrams, int quantity, string presentation, 
+        public Medicine(string id, string name, string description, string brand,
+            int dailyDosage, int milligrams, int quantity, string presentation,
             long servingsPerContainer, string? imageUrl, string? buyingLink)
         {
             Id = id;
