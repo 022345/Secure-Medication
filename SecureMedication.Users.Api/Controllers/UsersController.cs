d4690f65dfcb1a6fd0e1e8dca0c0dde4a8875918
Carlos <cjjosue03@gmail.com>
2026-08-14 22:18:40 -0400

    Added User controller

--- SecureMedication.Users.Api/Controllers/UsersController.cs
+++ SecureMedication.Users.Api/Controllers/UsersController.cs
@@ -1,14 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureMedication.Users.Api.Models;
+using SecureMedication.Users.Api.Persistence;
 
 namespace SecureMedication.Users.Api.Controllers
 {
     [Route("users")]
     public class GatewayController : Controller
     {
+
+        public readonly UsersDbContext _usersDbContext;
+
+        public GatewayController(UsersDbContext usersDbContext)
+        {
+            _usersDbContext = usersDbContext;    
+        }
+
         [Route("home")]
         public string home()
         {
             return "You're in the Gateway home point! Congrats it worked!";
+        }
+
+        [Route("seeUsers")]
+        public async Task<List<User>> showUsers([FromBody] List<User> users)
+        {
+            _usersDbContext.User.ToListAsync();
+            return users;
+        }
+
+        [Route("saveUsers")]
+        public async Task<List<User>> saveUsers([FromBody] List<User> users)
+        {
+            _usersDbContext.User.ToListAsync();
+            _usersDbContext.SaveChanges();
+            return users;
         }
     }
 }
