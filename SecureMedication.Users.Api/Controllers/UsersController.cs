c100e87784f54988484a3f774d42bae913c9d487
Carlos <cjjosue03@gmail.com>
2026-08-14 22:29:58 -0400

    Fixed controllers

--- SecureMedication.Users.Api/Controllers/UsersController.cs
+++ SecureMedication.Users.Api/Controllers/UsersController.cs
@@ -32,7 +32,7 @@ namespace SecureMedication.Users.Api.Controllers
         [Route("saveUsers")]
         public async Task<List<User>> saveUsers([FromBody] List<User> users)
         {
-            _usersDbContext.User.ToListAsync();
+            _usersDbContext.User.AddRangeAsync();
             _usersDbContext.SaveChanges();
             return users;
         }
