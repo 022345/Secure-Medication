dd32fb68a55f6eb7e1f50acec2f5e251b358530d
Carlos <cjjosue03@gmail.com>
2026-08-14 21:53:09 -0400

    Fixed issue 2

--- SecureMedication.Medicines.Api/Models/Medicine.cs
+++ SecureMedication.Medicines.Api/Models/Medicine.cs
@@ -9,46 +9,46 @@ namespace SecureMedication.Medicines.Api.Models
         // All the fields MUST be public with their { get; set; } so EF Core and JSON serializers can read/write them
 
         [Key] // Primary Key
-        [Column("id")]
+        [Column("Id")]
         public string Id { get; set; }
 
         [Required] // Sets column as NOT NULL
         [MaxLength(50)] // Defines maximum string length (VARCHAR(25))
-        [Column("name")]
+        [Column("Name")]
         public string Name { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(225)]
-        [Column("description")]
+        [Column("Description")]
         public string Description { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(50)]
-        [Column("brand")]
+        [Column("Brand")]
         public string Brand { get; set; } = string.Empty;
 
-        [Column("dailydosage")]
+        [Column("DailyDosage")]
         public int DailyDosage { get; set; }
 
-        [Column("milligrams")]
+        [Column("Milligrams")]
         public int Milligrams { get; set; }
 
-        [Column("quantity")]
+        [Column("Quantity")]
         public int Quantity { get; set; }
 
         [Required]
         [MaxLength(150)]
-        [Column("presentation")]
+        [Column("Presentation")]
         public string Presentation { get; set; } = string.Empty;
 
-        [Column("servingspercontainer")]
+        [Column("ServingsPerContainer")]
         public long ServingsPerContainer { get; set; }
 
         // The '?' operator allows the property to be NULLABLE in SQL without throwing C# compiler warnings
-        [Column("imageurl")]
+        [Column("ImageUrl")]
         public string? ImageUrl { get; set; }
 
-        [Column("buyinglink")]
+        [Column("BuyingLink")]
         public string? BuyingLink { get; set; }
 
         // EF Core requires a parameterless constructor to instantiate the model when querying the DB
@@ -57,21 +57,21 @@ namespace SecureMedication.Medicines.Api.Models
 
         }
 
-        public Medicine(string Id, string Name, string Description, string Brand,
-            int DailyDosage, int Milligrams, int Quantity, string Presentation,
-            long ServingsPerContainer, string? ImageUrl, string? BuyingLink)
+        public Medicine(string id, string name, string description, string brand,
+            int dailyDosage, int milligrams, int quantity, string presentation,
+            long servingsPerContainer, string? imageUrl, string? buyingLink)
         {
-            Id = Id;
-            Name = Name;
-            Description = Description;
-            Brand = Brand;
-            DailyDosage = DailyDosage;
-            Milligrams = Milligrams;
-            Quantity = Quantity;
-            Presentation = Presentation;
-            ServingsPerContainer = ServingsPerContainer;
-            ImageUrl = ImageUrl;
-            BuyingLink = BuyingLink;
+            Id = id;
+            Name = name;
+            Description = description;
+            Brand = brand;
+            DailyDosage = dailyDosage;
+            Milligrams = milligrams;
+            Quantity = quantity;
+            Presentation = presentation;
+            ServingsPerContainer = servingsPerContainer;
+            ImageUrl = imageUrl;
+            BuyingLink = buyingLink;
         }
     }
 }
