893e9bb85564cf262e0bd6d778855d8651f25df9
Carlos <cjjosue03@gmail.com>
2026-08-14 22:10:55 -0400

    Fixed issue 3

--- SecureMedication.Medicines.Api/Controllers/MedicineController.cs
+++ SecureMedication.Medicines.Api/Controllers/MedicineController.cs
@@ -5,7 +5,8 @@ using System.Net;
 using SecureMedication.Medicines.Api.Persistence;
 
 //Need to use the namespace for the Models package in order to access the classes models 
-using SecureMedication.Medicines.Api.Models; 
+using SecureMedication.Medicines.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SecureMedication.Medicines.Api.Controllers
 {
@@ -27,8 +28,21 @@ namespace SecureMedication.Medicines.Api.Controllers
             return "Api functional";
         }
 
+        [Route("seeMedicine")]
+        public List<Medicine> showMedicines([FromBody] List<Medicine> medicine)
+        {
+            _context.Medicine.ToListAsync();
+            return medicine;
+        }
+
+
         [Route("saveMedicine")]
         //Need to be for the actual endpoint: public List<Medicine> SaveMedicines(Medicine medicine)
+
+        /*It's mandatory to add [FromBody]. [FromBody] le indica a ASP.NET Core que extraiga y 
+           convierta automáticamente los datos en formato JSON enviados en el cuerpo de la petición 
+           HTTP (el body de Postman) para asignarlos al parámetro del método.*/
+
         public async Task <List<Medicine>> saveMedicines([FromBody] List<Medicine> medicine)
         {
             _context.Medicine.AddRangeAsync(medicine);
