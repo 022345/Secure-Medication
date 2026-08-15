b4d26afe9bb779efcdf48c93715c1670fa7e2383
Carlos <cjjosue03@gmail.com>
2026-08-14 22:13:13 -0400

    Fixed issue 4

--- SecureMedication.Users.Api/Models/User.cs
+++ SecureMedication.Users.Api/Models/User.cs
@@ -8,14 +8,17 @@ namespace SecureMedication.Users.Api.Models
     public class User
     {
         [Key] // Primary Key definition
+        [Column("userId")]
         public int UserId { get; set; }
 
         [Required]
         [MaxLength(50)]
+        [Column("userName")]
         public string UserName { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(255)] // A long length is useful to store password hashes (BCrypt/Argon2) instead of plain text
+        [Column("password")]
         public string Password { get; set; } = string.Empty;
 
         // In microservices, do NOT use navigation properties ([ForeignKey] / virtual Medicine) across different databases.
